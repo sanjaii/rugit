63e4d395091933cd207031c0a57f13582eda77d6
Sanjay Karukamanna <sanjay.work@hotmail.com>
2022-01-15 21:41:55 +0530

    Added hash_object function. Fixed rubocop warnings

--- main.rb
+++ main.rb
@@ -1,21 +1,33 @@
-class Rgit
+# frozen_string_literal: true
+
+require 'digest/sha1'
+
+# All the rugit functionalities Implemented in this class
+class Rugit
   GIT_DIR = '.rugit'
+
   def initialize(args)
     @args = args
-    if args[0] == "init"
-      init()
-    end
+    init if args[0] == 'init'
+    hash_object if args[0] == 'hash-object'
   end
+
   def init
-    if File.exists?(GIT_DIR)
-      p "Already a rgit repository"
+    if File.exist?(GIT_DIR)
+      p 'Already a rgit repository'
     else
       Dir.mkdir GIT_DIR
-      p "Initialized a git repository"
+      Dir.mkdir "#{GIT_DIR}/objects"
+      p 'Initialized a git repository'
     end
+  end
+
+  def hash_object
+    return if @args[1].nil?
+
+    oid = Digest::SHA1.hexdigest @args[1]
+    File.open("#{GIT_DIR}/objects/#{oid}", 'wb')
   end
 end
 
-if $0 == __FILE__
-  Rgit.new(ARGV)
-end
+Rugit.new(ARGV) if $PROGRAM_NAME == __FILE__
